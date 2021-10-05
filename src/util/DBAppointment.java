98be1b2e1e7abe3aec76a79442f5ce2ccfd98311
dtheis06 <vizine@live.com>
2021-10-04 20:15:44 -0400

    Logout Button

--- src/util/DBAppointment.java
+++ src/util/DBAppointment.java
@@ -1,10 +1,18 @@
 package util;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TableView;
 import javafx.scene.control.ToggleGroup;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class DBAppointment {
 
@@ -35,5 +43,16 @@ public class DBAppointment {
     @FXML
     private RadioButton weekRadio;
 
+    public void setLogoutButton(ActionEvent event) {
+        try {
+            Parent parent = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+            Scene scene = new Scene(parent);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.getStackTrace();
+        }
+    }
 }
 
